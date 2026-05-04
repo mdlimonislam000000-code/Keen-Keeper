3425ec43a9dc17ab76c7f2272095d29e32fc9081
mdlimonislam000000 <mdlimonislam000000@gmail.com>
2026-05-05 00:53:39 +0600

    recherts add

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+'use client'
+import { CallContext } from '@/context/callcontext';
+import { useContext } from 'react';
+import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 
-const StatsPage = () => {
+
+const page = () => {
+
+    const { callData } = useContext(CallContext)
+
+    const totalCalls = callData.filter(item => item.type === 'Call').length;
+    const totalTexts = callData.filter(item => item.type === 'Text').length;
+    const totalVideos = callData.filter(item => item.type === 'Video').length;
+
+
+    const data = [
+        { name: 'Calls', value: totalCalls, fill: '#0088FE' },
+        { name: 'Texts', value: totalTexts, fill: '#00C49F' },
+        { name: 'Videos', value: totalVideos, fill: '#FFBB28' },
+
+    ];
+
+
     return (
-        <div>
-            <p className="text-center text-4xl mt-72">This is stats page</p>
+        <div className='container mx-auto'>
+            <p className='text-center text-[#1f2937] text-[3rem]  mt-10 font-bold'>Friendship Analytics</p>
+           
+
+            <div className=' shadow-2xl  rounded flex mx-auto flex-col my-10' style={{ width: '100%', maxWidth: '500px', aspectRatio: 1 }}>
+
+                <div>
+                    <p className='text-[1.2rem] font-bold text-[#244d3f] text-center mb-5'>By Interaction Type</p>
+                </div>
+                <ResponsiveContainer width="100%" height="100%">
+                    
+                    <PieChart>
+                        
+                        <Pie
+                            data={data}
+                            innerRadius="80%"
+                            outerRadius="100%"
+                            cornerRadius="50%"
+                            paddingAngle={5}
+                            dataKey="value"
+                            isAnimationActive={true}
+                        />
+                        <Legend></Legend>
+                        <Tooltip></Tooltip>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
 
-export default StatsPage;
+export default page;
